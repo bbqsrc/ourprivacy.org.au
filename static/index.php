35a8d3b93731ee4f28a30ac3312e16ffcd149294
Brendan Molloy <brendan@bbqsrc.net>
2013-06-18 20:51:26 +1000

    static

---
+++ static/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset="utf-8">
+    <title>Protect Our Privacy - National Day of Action, Saturday July 6</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <!--[if lt IE 9]><script src="http://ourprivacy.org.au/wp-content/themes/brendan/html5shiv.js"></script><![endif]-->
+    <!--[if lt IE 8]>
+<link rel='stylesheet' id='brendan-shit-browsers-css'  href='http://ourprivacy.org.au/wp-content/themes/brendan/shit-browsers.css?ver=0.1' type='text/css' media='all' />
+<![endif]-->
+    <link rel='stylesheet' id='brendan-style-css'  href='http://ourprivacy.org.au/wp-content/themes/brendan/style.css?ver=0.1' type='text/css' media='all' />
+    
+  </head>
+  <body>
+    <div id="page">
+      <h1><a href="http://ourprivacy.org.au">Protect Our Privacy</a></h1>
+      <h2>National Day of Action &mdash; Saturday, July 6</h2>
+
+      <p>[something about NSA metadata etc]</p>
+
+      <p>Information about events</p>
+
+      <p>This website is still being prepared, so join our mailing list for updates and we'll let you know when  content is added to the site and updates on the events in your area!</p>
+
+      <form action="http://lists.pirateparty.org.au/cgi-bin/dada/mail.cgi" method="post">
+      <input type="hidden" name="list" value="ourprivacy">
+      <input type="hidden" name="f" value="subscribe">
+      <label>Email: <input type="email" name="email"></label>
+      <input type="submit">
+      </form>
+
+    </div>
+  </body>
+</html>
