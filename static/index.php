abf61d8f29b7901fb3eee9926e904655217200f4
Brendan Molloy <brendan@bbqsrc.net>
2013-06-20 23:42:54 +1000

    fuu

--- static/index.php
+++ static/index.php
@@ -9,7 +9,7 @@
 <link rel='stylesheet' href='http://ourprivacy.org.au/wp-content/themes/brendan/shit-browsers.css?ver=0.1' type='text/css' media='all' />
 <![endif]-->
     <link rel='stylesheet'  href='foo.css?ver=20080620-1' type='text/css' media='all'>
-    <link rel="image_src" href="img/logo.png">
+    <link rel="image_src" href="http://ourprivacy.org.au/img/logo.png">
   </head>
   <body>
     <div id="page">
